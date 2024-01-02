10b5d89b6e44aaeb41b07c9afa9a289d2efbe3c3
dontyellwillcry <papaporo@proton.me>
2024-01-02 15:56:47 -0600

    first commit

---
+++ app/settings.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useRouter } from "expo-router";
+
+export default function Settings() {
+    const navigation = useRouter();
+  return (
+    <View style={styles.container}>
+      <Text>Settings!</Text>
+      <Button
+        title="Go to Home!"
+        onPress={() => navigation.replace("/")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
